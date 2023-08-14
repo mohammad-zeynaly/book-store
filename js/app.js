6501e56a6c3fff207772c59459707ab7b1b0f19c
Mohammad Zeynali <mhmdzynaly977@gmail.com>
2023-08-14 22:20:36 +0330

    The navigation section was developed in mobile mode

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 "use strict";
+// select element in dom
+const overlayContainer = document.querySelector("#overlay");
+const mobileMenuContainer = document.querySelector(".mobile-menu");
+const mobileMenuBtn = document.querySelector("#mobile-nav__btn");
+const mobileMenuCloseBtn = document.querySelector(".mobile-menu__close-btn");
+// is show mobile menu
+
+const overlayShowHandler = () => {
+  overlayContainer.classList.remove("overlay");
+  mobileMenuContainer.classList.remove("mobile-menu--open");
+};
+
+const mobileMenuShowHandler = () => {
+  mobileMenuContainer.classList.toggle("mobile-menu--open");
+  overlayContainer.classList.toggle("overlay");
+};
+
+// set events
+overlayContainer.addEventListener("click", overlayShowHandler);
+mobileMenuBtn.addEventListener("click", mobileMenuShowHandler);
+mobileMenuCloseBtn.addEventListener("click", overlayShowHandler);
